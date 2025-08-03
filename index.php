51c55e090083b667a50e94cc3b532a84c453040d
petraalpigiano <petraalpigiano@gmail.com>
2025-08-03 12:03:37 +0200

    add class Movie

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<?php
+class Movie
+{
+    public $name;
+    public $director;
+    public $actor;
+    public $year;
+
+    function __constructor($_name, $_director, $_actor, $_year)
+    {
+        $this->name = $_name;
+        $this->director = $_director;
+        $this->actor = $_actor;
+        $this->year = $_year;
+    }
+    public function getYear($year)
+    {
+        if ($year >= 2010) {
+            echo "Il film è recente";
+        } else {
+            echo "Il film non è recente";
+        }
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
